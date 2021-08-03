8042c0c4f576f7bb9f79840060d66362220da1e7
sachoker <mv1492412131@gmail.com>
2021-08-03 22:01:28 +0300

    first

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,8 +3,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 
 sched = BackgroundScheduler()
-client_id = "-iJiRUw8U1NukcfLAFzg"
-client_secret = "QRdEx3eJAWYrLvbloIZZNdQWqnDGrPi0fYNoqJyW"
+client_id = "t7ivBRaOzMEhvubvwa4U"
+client_secret = "1xLUhklG6-FgIDWD0Zqb0YAt3tOXQgoEDZwR1YY_"
 
 bot = AvitoBot(client_id, client_secret)
 bot.get_webhooks()
