4035c3aaa49194978ff9f1a7625f0dbf24733629
sachoker <mv1492412131@gmail.com>
2021-08-03 20:37:07 +0300

    first

--- bot/AvitoBot.py
+++ bot/AvitoBot.py
@@ -8,10 +8,9 @@ class AvitoBot:
         self.client_secret = client_secret
         self.avitoapikey = None
         self.get_avito_key()
-        with open('names.txt', 'w') as f:
-            for i in self.get_all_chats('169306001'):
-                f.write(i['id'] + '\n')
-            f.close()
+        self.names = []
+        for i in self.get_all_chats('169306001'):
+            self.names.append(i['id'])
 
 
     def get_avito_key(self):
@@ -32,20 +31,14 @@ class AvitoBot:
                    json=payload)
 
     def message_handler(self, chat_id, user_id, text):
-        with open('names.txt', 'r') as f:
-            names = f.readlines()
-            f.close()
-        if chat_id + '\n' not in names:
-            names.append(chat_id + '\n')
+        if chat_id not in self.names:
+            self.names.append(chat_id)
             self.send_message(chat_id, user_id, 'Здравствуйте')
             sleep(2)
             self.send_message(chat_id, user_id,
                               'Если Вы это не написали, укажите марку модель, год выпуска автомобиля и высоту проставок.')
             sleep(2)
             self.send_message(chat_id, user_id, 'Я чуть позже пришлю фото и цены')
-            with open('names.txt', 'w') as f:
-                f.writelines(names)
-                f.close()
             self.read_chat(chat_id, user_id)
         return 1
 
